e59114edac5ce62fafeaade9a44963a961e44d51
Robert Edger <rbedger@gmail.com>
2022-02-08 08:43:52 -0600

    test all bulbs

--- lamp.ino
+++ lamp.ino
@@ -6,9 +6,23 @@
 #define COLOR_ORDER GRB
 #define DEFAULT_BRIGHTNESS 255
 
-#define NUM_LEDS 1
+#define NUM_LEDS 11
 
 CRGB leds[NUM_LEDS];
+int counter = 0;
+CRGB colors[NUM_LEDS] = {
+    CRGB::AliceBlue,
+    CRGB::Amethyst,
+    CRGB::AntiqueWhite,
+    CRGB::Aqua,
+    CRGB::Aquamarine,
+    CRGB::Azure,
+    CRGB::Beige,
+    CRGB::Bisque,
+    CRGB::Black,
+    CRGB::BlanchedAlmond,
+    CRGB::Blue,
+};
 
 void setup()
 {
@@ -24,11 +38,15 @@ void loop()
 {
   Serial.println("loop");
 
-  leds[0] = CRGB::White;
+  for (int i =0; i < NUM_LEDS; i++) {
+    leds[i] = colors[(counter + i) % NUM_LEDS];
+  }
+
   FastLED.show();
   delay(300);
 
-  leds[0] = CRGB::Red;
-  FastLED.show();
-  delay(300);
+  if (++counter >= NUM_LEDS)
+  {
+    counter = 0;
+  }
 }
